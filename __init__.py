39f71a8c9fd167ba7f942fb0c92b3233b02d5f95
Harrison Nordby <fishcreekblend@gmail.com>
2014-10-26 18:00:43 -0500

    fix version number

--- __init__.py
+++ __init__.py
@@ -40,7 +40,7 @@ bl_info = {
     "name": "BEPUik Tools",
     "author": "Harrison Nordby, Ross Nordby",
     "version": (0, 4, 0),
-    "blender": (2, 72, 1),
+    "blender": (2, 72, 0),
     "description": "Automatically create humanoid BEPUik rigs and other rigging knickknacks.",
     "location": "View3D > Tool Shelf > BEPUik and View3D > Properties Shelf",
     "warning": "",
